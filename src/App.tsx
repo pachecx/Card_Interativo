b6c29142660b080386627d40011fed51acb5f8e8
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-03-15 17:15:40 -0300

    ajuste renderização inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import imgCardOk from './assets/icon-complete.svg';
 import imgMobile from "./assets/bg-main-mobile.png";
 import backCard from './assets/bg-card-back.png';
 import frontCard from './assets/bg-card-front.png';
@@ -25,18 +26,37 @@ export function App() {
 });
   
   const [name, setName] = useState<string>('');
-  const [cardNumber, setCardNumber] = useState<string>();
+  const [cardNumber, setCardNumber] = useState<string>('');
   const [dateM, setDateM] =  useState<string>();
   const [dateY, setDateY] =  useState<string>();  
 
   const [codSafe, setCodSafe] = useState<string>();
 
+  const [cardOk, setCardOk] = useState<boolean>(false);
+
   const onSubmit = () => {
-
-
-    console.log(name)
+    console.log('deu certo')
+    setCardOk(true)
+    reset();
   }
 
+  const resetForm = () => {
+    setCardOk(false);
+    console.log('voltou');
+    console.log(cardOk)
+  }
+
+  const renderCardNumber = () => {
+    if (!cardNumber) return ''; // Se não houver número de cartão, não há nada para exibir
+
+    const groupedNumbers = [];
+    for (let i = 0; i < cardNumber.length; i += 4) {
+      groupedNumbers.push(cardNumber.slice(i, i + 4));
+    }
+    return groupedNumbers.join(' ');
+  };
+
+  console.log(renderCardNumber())
 
   return ( 
     <div className='page'>
@@ -60,86 +80,113 @@ export function App() {
                 className='logoCard'
                 src={cardlogo}
               />
-              <span className='cardNumber'>{cardNumber}</span>
+              <span className='cardNumber'>{renderCardNumber()}</span>
               <div className='textName'>
                 <p>{name}</p>
-                <p>00/00</p>
+                <p>{dateM}/{dateY}</p>
               </div>
             </div>
           </div>
           
         </div>
       </div>
+
+
+
+
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="inputs">
-          <label >CARDHOLDER NAME</label>
-          <input
-            type='text'
-            placeholder='e.g. Jane Appleseed'
-            
-            //onChange={(e) => setName(e.target.value)}
-            {...register('name')}
-          />
-          {errors.name && <span className='alertInput'>{errors.name.message}</span>}
-        </div>
-        <div className="inputs">
-          <label htmlFor='number'>CARD NUMBER</label>
-          <input
-            type='text'
-            placeholder='e.g. 123 456 789'
-            name='number'
-            onChange={(e) => setCardNumber(e.target.value)}
-          />
-          {errors.number && <span className='alertInput'>{errors.number.message}</span>}
-        </div>
+        {cardOk === false ? (
+          <div>
+            <div className="inputs">
+              <label htmlFor='name'>CARDHOLDER NAME</label>
+              <input
+                type='text'
+                placeholder='e.g. Jane Appleseed'
+                {...register('name')}
+                className={errors.codSafe ? "error" : ""}
+                name='name'
+                onChange={(e) => setName(e.target.value)}
+              />
+              {errors.name && <span className='alertInput'>{errors.name.message}</span>}
+            </div>
+            <div className="inputs">
+              <label htmlFor='number'>CARD NUMBER</label>
+              <input
+                type='number'
+                placeholder='e.g. 123 456 789'
+                {...register('number', {valueAsNumber: true})}
+                className={errors.codSafe ? "error" : ""}
+                name='number'
+                onChange={(e) => setCardNumber(e.target.value)}
+              />
+              {errors.number && <span className='alertInput'>{errors.number.message}</span>}
+            </div>
 
-        <div className='allDates'>
-          <div className='inputsDate'>
-            <label htmlFor='mes'>EXP. DATE</label>
+            <div className='allDates'>
+              <div className='inputsDate'>
+                <label htmlFor='mes'>EXP. DATE</label>
+                <input 
+                  type='number'
+                  placeholder='MM'
+                  {...register('expDateM', {valueAsNumber: true})}
+                  className={errors.codSafe ? "error" : ""}
+                  name='expDateM'
+                  onChange={(e) => setDateM(e.target.value)}
+                />
+                {errors.expDateM && <span className='alertInput'>{errors.expDateM.message}</span>}
+
+              </div>
+
+              <div className='inputsDate'>
+                <label htmlFor='expDateY'>(MM/YY)</label>
+                <input 
+                  type='number'
+                  placeholder='YY'
+                  {...register('expDateY', {valueAsNumber: true})}
+                  className={errors.codSafe ? "error" : ""}
+                  name='expDateY'
+                  onChange={(e) => setDateY(e.target.value)}
+                />
+                {errors.expDateY && <span className='alertInput'>{errors.expDateY.message}</span>}
+              </div>
+
+              <div className='inputsDate'>
+                <label htmlFor='codSafe'>CVC</label>
+                <input 
+                  type='number'
+                  placeholder='e. g. 123'
+                  {...register('codSafe', {valueAsNumber: true})}
+                  className={errors.codSafe ? "error" : ""}
+                  name='codSafe'
+                  onChange={(e) => setCodSafe(e.target.value)}
+                />
+                {errors.codSafe && <span className='alertInput'>{errors.codSafe.message}</span>}
+              </div>
+            </div>
+
             <input 
-              maxLength={2}
-              type='text'
-              placeholder='MM'
-              name='mes'
-              onChange={(e) => setDateM(e.target.value)}
+              value="Confim"
+              className='bntEnviar'
+              type='submit'
             />
-            {errors.expDateM && <span className='alertInput'>{errors.expDateM.message}</span>}
-
           </div>
-
-          <div className='inputsDate'>
-            <label htmlFor='ano'>(MM/YY)</label>
+        ):( 
+          <div className="ok">
+            <img
+              src={imgCardOk}
+              alt=''
+            />
+            <h1>Thank you !</h1>
+            <p>We've added your card details</p>
             <input 
-              maxLength={2}
-              type='text'
-              placeholder='YY'
-              name='ano'
-              onChange={(e) => setDateY(e.target.value)}
+              value="Continue"
+              className='bntEnviar'
+              type='button'
+              onClick={resetForm}
             />
-            {errors.expDateY && <span className='alertInput'>{errors.expDateY.message}</span>}
           </div>
-
-          <div className='inputsDate'>
-            <label htmlFor='cvc'>CVC</label>
-            <input 
-              maxLength={3}
-              type='text'
-              placeholder='e. g. 123'
-              name='cvc'
-              onChange={(e) => setCodSafe(e.target.value)}
-            />
-            {errors.codSafe && <span className='alertInput'>{errors.codSafe.message}</span>}
-          </div>
-        </div>
-
-        <input 
-          value="Confim"
-          className='bntEnviar'
-          type='submit'
-        />
+        )}
       </form>
     </div>
   );
 };
-
-
--- src/schema/card.schema.ts
+++ src/schema/card.schema.ts
@@ -1,11 +1,29 @@
-import { object, string, z } from 'zod';
+import { z } from 'zod';
 
 export const cardRegister = z.object({
-    name: z.string().min(4, "Quatro caracteres"),
-    number: z.string().min(6, "precisa ser seis numeros"),
-    expDateM: z.string().min(2, "Precisa ser dois Digitos"),
-    expDateY: z.string().min(2, "Precisa ser dois Digitos"), 
-    codSafe: z.string().min(3, "Precisa ser 3 Digitos"), 
+    name: z.string().regex(/^[a-zA-ZÀ-ÿ]+(?:\s+[a-zA-ZÀ-ÿ]+)+$/, {
+        message: 'Full name',
+    }),
+    number: z.number({required_error: "", invalid_type_error: "Can'be Blank"})
+    .min(16, "Must be sixteen numbers").max(16, "Must be sixteen numbers"),
+
+    expDateM: z.number({required_error: "", invalid_type_error: "Can'be Blank"})
+    .min(2, "Must be two numbers").max(2, "Must be two numbers"),
+   
+    expDateY: z.number({required_error: "", invalid_type_error: "Can'be Blank"})
+    .min(2, "Must be two numbers").max(2, "Must be two numbers"),
+
+    codSafe: z.number({required_error: "", invalid_type_error: "Can'be Blank"})
+    .min(3, "Must be three numbers").lt(3), 
 })
 
 export type cardRequestData = z.infer<typeof cardRegister>
+
+
+
+// formatar numero do cartao
+
+// tirar o / de entre as datas
+// pagina da f5 ao fechar modal
+// add imagem desktop 1024px
+// alterar todos os tipos para number
